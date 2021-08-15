2b0d6cb0f3979a9aee3c7303bd174f9e39858f11
Ronni Hartlage <hartlage.7@wright.edu>
2021-08-15 18:58:46 -0400

    3.8 - configurning morgan to see POST request info

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
+
+morgan.token('content', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons = [
     { 
